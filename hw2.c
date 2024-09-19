b25bb9b5c887c52514cf69d4893e0ae6dd6cf9f8
ggoons1106 <ggoon1106@gmail.com>
2024-09-19 22:19:34 +0900

    Add files via upload

---
+++ hw2.c
@@ -0,0 +1,11 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	float num1 ;
+	printf("Please enter kilometers:");
+	scanf("%f", &num1);
+	printf("%.1f km is equal to %.1f miles", num1, num1 / 1.609);
+	return 0;
+}
