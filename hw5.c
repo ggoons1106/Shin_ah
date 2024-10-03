2c0d1c6185e257c432e4994c2ce4ebe3c8d8c285
ggoons1106 <ggoon1106@gmail.com>
2024-10-03 21:24:20 +0900

    Add programming file [hw5.c]

--- hw5.c
+++ hw5.c
@@ -1,1 +1,19 @@
-﻿
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num1, num3 = 0;
+	printf("Please enter a number:");
+	scanf("%d", &num1);
+	for (int num2 = 1; num2 <= num1; num2++)
+	{
+		if (num1 % num2 == 0)
+			num3 += 1;
+	}
+	if (num3 == 2)
+		printf("It is a prime number");
+	else
+		printf("It is not a prime number");
+	return 0;
+}
